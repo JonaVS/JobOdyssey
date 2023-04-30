005fdbdb220f90a6ed3622d07f3da311200998e4
JonaVS <jon4963@gmail.com>
2023-04-30 01:42:02 -0600

    feat: add Mantine UI lib base config

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,23 @@
-import '@/styles/globals.css';
-import type { AppProps } from 'next/app';
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+import Head from "next/head";
+import { MantineProvider } from "@mantine/core";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <title>Job Odyssey</title>
+        <meta
+          name="viewport"
+          content="minimum-scale=1, initial-scale=1, width=device-width"
+        />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <MantineProvider withGlobalStyles withNormalizeCSS>
+        <Component {...pageProps} />
+      </MantineProvider>
+    </>
+  );
 }
