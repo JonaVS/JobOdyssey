ea9b532115831b6a0809ce85948c438929b1b013
JonaVS <jon4963@gmail.com>
2023-05-06 18:43:35 -0600

    feat: hide navbar content if theres no user
    -Navbar content is going to be used as a list for user job boards names

--- src/components/AppShell/AppShell.tsx
+++ src/components/AppShell/AppShell.tsx
@@ -1,16 +1,19 @@
 import { ReactNode } from "react";
 import { AppShell } from "@mantine/core";
 import { useToggle } from '@mantine/hooks';
-import useStyles from "./AppShell.styles";
+import { useAppSelector } from "@/hooks/reduxHooks";
+import { selectUser } from "@/store/slices/userAuthSlice";
 import AppShellHeader from "./AppShellHeader/AppShellHeader";
 import AppShellNavbar from "./AppShellNavbar/AppShellNavbar";
 import AppShellFooter from "./AppShellFooter/AppShellFooter";
+import useStyles from "./AppShell.styles";
 
 type Props = {
   children: ReactNode;
 };
 
 function AppMainShell({ children }: Props) {
+  const user = useAppSelector(selectUser);
   const { classes } = useStyles();
   const [opened, toggleMenu] = useToggle()
 
@@ -20,7 +23,7 @@ function AppMainShell({ children }: Props) {
       navbarOffsetBreakpoint="sm"
       asideOffsetBreakpoint="sm"
       header={<AppShellHeader opened={opened} showMenu={toggleMenu} />}
-      navbar={<AppShellNavbar hidden={!opened} />}
+      navbar={user ? <AppShellNavbar hidden={!opened} /> : <div/>}
       footer={<AppShellFooter />}
     >
       {children}
