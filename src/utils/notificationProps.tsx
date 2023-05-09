fa0c53bc4e7411e52d7bd5abc9e7095f67a43505
JonaVS <jon4963@gmail.com>
2023-05-08 22:07:43 -0600

    feat: add getNotificationProps helper method
    -Used to generate Mantine notifications

---
+++ src/utils/notificationProps.tsx
@@ -0,0 +1,38 @@
+import { NotificationProps } from "@mantine/notifications";
+import { IconCheck, IconExclamationMark, IconX } from "@tabler/icons-react";
+
+export enum NotificationType {
+  SUCCESS = "success",
+  ERROR = "error",
+  WARNING = "warning",
+}
+
+type Notifications = Record<NotificationType, NotificationProps>;
+
+const iconSize = "1.1rem";
+
+const notificationProps: Notifications = {
+  success: {
+    color: "teal",
+    message: "Default success message",
+    icon: <IconCheck size={iconSize} />,
+  },
+  error: {
+    color: "red",
+    message: "Default error message",
+    icon: <IconX size={iconSize} />,
+  },
+  warning: {
+    color: "yellow",
+    message: "Default warning message",
+    icon: <IconExclamationMark size={iconSize} />,
+  },
+};
+
+function getNotificationProps(type: NotificationType, message?: string) {
+  const props = notificationProps[type];
+  props.message = message;
+  return props;
+}
+
+export default getNotificationProps;
