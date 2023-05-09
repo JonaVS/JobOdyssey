f9b7371d56017a7a06a99c61824e0edb0e9eccfd
JonaVS <jon4963@gmail.com>
2023-05-08 22:24:37 -0600

    ui: place notifications to bottom-center

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -18,7 +18,7 @@ export default function App({ Component, pageProps }: AppProps) {
       </Head>
       <Providers>
         <Layout>
-          <Notifications />
+          <Notifications position="bottom-center" />
           <Component {...pageProps} />
         </Layout>
       </Providers>
