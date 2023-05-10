5f812c40d91f8a53ea1a75dc732a1b767fb4287c
JonaVS <jon4963@gmail.com>
2023-05-10 12:40:59 -0600

    feat: prevent users go to auth page if loggedIn
    -Server side rendering is not being used so this is needed

--- src/pages/auth.tsx
+++ src/pages/auth.tsx
@@ -1,7 +1,23 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import Head from "next/head";
 import AuthPanel from "@/components/AuthPanel/AuthPanel";
+import { useAppSelector } from "@/hooks/reduxHooks";
+import { useRouter } from "next/router";
+import { selectUser } from "@/store/slices/userAuthSlice";
+import { useEffect } from "react";
 
 export default function Auth() {
+  const user = useAppSelector(selectUser);
+  const router = useRouter();
+
+  useEffect(() => {
+    if (user) {
+      router.push("/");
+    }
+  }, []);
+
+  if (user) return null;
+
   return (
     <>
       <Head>
