7fdd1962ef49477efbdbd8a8b64ab3b146ad218a
JonaVS <jon4963@gmail.com>
2023-05-11 17:44:34 -0600

    feat: add refresh token interceptor
    -Clunky during dev if reactStrictMode is true in next.config.js file

--- src/api/agent.ts
+++ src/api/agent.ts
@@ -1,7 +1,8 @@
-import axios, { AxiosResponse } from "axios";
+import axios, { AxiosError, AxiosResponse, InternalAxiosRequestConfig } from "axios";
 import { RegisterRequest, AuthResponse, LoginRequest, User } from "@/models/auth/authModels";
 
 const response = <T>(res: AxiosResponse<T>) => res.data;
+let isRefreshingJwt: boolean = false;
 
 axios.defaults.baseURL = process.env.NEXT_PUBLIC_BASE_API_URL;
 axios.defaults.withCredentials = true;
@@ -39,6 +40,45 @@ if (process.env.NODE_ENV === "development") {
 /*
     -----DEV MODE ONLY CODE END-----
 */
+
+
+/*
+    -----REFRESH TOKEN-----
+*/
+axios.interceptors.response.use(
+  (res: AxiosResponse) => res,
+  async (error: AxiosError) => {
+    const originalRequest = error.config as InternalAxiosRequestConfig;
+
+    if (
+      error.response?.status === 401 &&
+      !isRefreshingJwt &&
+      localStorage.getItem("jwt")
+    ) {
+      isRefreshingJwt = true;
+
+      try {
+        // Refresh token is inside http only cookie.
+        // Thats why no payload.
+        const refreshResponse = await axios
+          .post<AuthResponse>(`/auth/refresh-token`)
+          .then(response);
+
+        localStorage.setItem("jwt", refreshResponse.token);
+
+        originalRequest.headers.Authorization = `Bearer ${refreshResponse.token}`;
+
+        return await axios(originalRequest);
+
+      } catch (err) {
+        /* do nothing  */
+      } finally {
+        isRefreshingJwt = false;
+      }
+    }
+    return Promise.reject(error);
+  }
+);
 
 const auth = {
   register: (body: RegisterRequest) => axios.post<AuthResponse>("/auth/register", body).then(response),
