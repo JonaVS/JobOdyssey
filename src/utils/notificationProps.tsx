c2ac3058b42e8d74be59cd5d84dbae3da4e51abc
JonaVS <jon4963@gmail.com>
2023-05-19 17:16:04 -0600

    refactor: improve getNotificationProps helper method
    -Removed default messages
    -A message needs to be passed

--- src/utils/notificationProps.tsx
+++ src/utils/notificationProps.tsx
@@ -7,30 +7,27 @@ export enum NotificationType {
   WARNING = "warning",
 }
 
-type Notifications = Record<NotificationType, NotificationProps>;
+type Notifications = Record<NotificationType, Partial<NotificationProps>>;
 
 const iconSize = "1.1rem";
 
 const notificationProps: Notifications = {
   success: {
     color: "teal",
-    message: "Default success message",
     icon: <IconCheck size={iconSize} />,
   },
   error: {
     color: "red",
-    message: "Default error message",
     icon: <IconX size={iconSize} />,
   },
   warning: {
     color: "yellow",
-    message: "Default warning message",
     icon: <IconExclamationMark size={iconSize} />,
   },
 };
 
-export function getNotificationProps(type: NotificationType, message?: string) {
+export function getNotificationProps(type: NotificationType, message: string):NotificationProps {
   const props = notificationProps[type];
   props.message = message;
-  return props;
+  return props as NotificationProps;
 }
